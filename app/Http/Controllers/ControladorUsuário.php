f8ae4dbc0e08afb393ab21e303057e9b81d7a397
Viniciusm15 <viniciushiagomartins00@gmail.com>
2021-06-25 22:35:02 -0300

    feat: controlador de usuário, models e rota feitas

--- app/Http/Controllers/ControladorUsuário.php
+++ app/Http/Controllers/ControladorUsuário.php
@@ -3,82 +3,106 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Course;
+use App\Models\TicketForm;
 
 class ControladorUsuário extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $usuarios = User::all();
+
+        return view('usuarios', compact('usuarios'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('novousuario');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $usuario = new User();
+        $usuario->matricula = $request->input('matriculaUsuario');
+        $usuario->nacionalidade = $request->input('nacionalidadeUsuario');
+        $usuario->data_nascimento = $request->input('data_nascimentoUsuario');
+        $usuario->email = $request->input('emailUsuario');
+        $usuario->sexo = $request->input('sexoUsuario');
+        $usuario->raca = $request->input('racaUsuario');
+        $usuario->forma_ingresso_id = $request->input('forma_ingresso_idUsuario');
+        $usuario->curso_id = $request->input('curso_idUsuario');
+        $usuario->status = $request->input('statusUsuario');
+        $usuario->senha = $request->input('senhaUsuario');
+
+        $usuario->save();
+
+        return redirect('/usuarios');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $usuario = User::find($id);
+        if (isset($usuario))
+        {
+            return view('editarusuario', compact('usuario'));
+        }
+
+        return redirect('/usuarios');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $usuario = User::find($id);
+        if (isset($usuario))
+        {
+            $usuario->matricula = $request->input('matriculaUsuario');
+            $usuario->nacionalidade = $request->input('nacionalidadeUsuario');
+            $usuario->data_nascimento = $request->input('data_nascimentoUsuario');
+            $usuario->email = $request->input('emailUsuario');
+            $usuario->sexo = $request->input('sexoUsuario');
+            $usuario->raca = $request->input('racaUsuario');
+            $usuario->forma_ingresso_id = $request->input('forma_ingresso_idUsuario');
+            $usuario->curso_id = $request->input('curso_idUsuario');
+            $usuario->status = $request->input('statusUsuario');
+            $usuario->senha = $request->input('senhaUsuario');
+
+            $usuario->save();
+        }
+
+        return redirect('/usuarios');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $usuario = User::find($id);
+        if (isset($usuario))
+        {
+            $usuario->delete();
+        }
+
+        return redirect('/usuarios');
+    }
+
+    public static function getCourseName($id)
+    {
+        $usuario = User::find($id);
+        if (isset($usuario))
+        {
+            $curso = Course::find($curso['curso_id']);
+            if (isset($curso))
+                return $curso->curso;
+        }
+    }
+
+    public static function getTicketForm($id)
+    {
+        $usuario = User::find($id);
+        if (isset($usuario))
+        {
+            $formaIngresso = TicketForm::find($formaIngresso['forma_ingresso_id']);
+            if (isset($formaIngresso))
+                return $formaIngresso->forma_ingresso;
+        }
     }
 }
---
+++ resources/views/user/usuarios.blade.php
@@ -0,0 +1,58 @@
+<?php use App\Http\Controllers\ControladorUsuário; ?>
+
+@extends('layout.app', ["current" => "usuarios"])
+
+@section('body')
+<div class="card border">
+    <div class="card-body">
+        <h5 class="card-title">Cadastro de Usuários</h5>
+        @if(count($usuarios) > 0)
+        <table class="table table-ordered table-hover">
+            <thead>
+                <tr>
+                    <th>Mátricula</th>
+                    <th>Nacionalidade</th>
+                    <th>Data_Nascimento</th>
+                    <th>Email</th>
+                    <th>Sexo</th>
+                    <th>Raça</th>
+                    <th>Forma_Ingresso</th>
+                    <th>Curso</th>
+                    <th>Status</th>
+                    <th>Senha</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($usuarios as $user)
+                <tr>
+                    <td>{{$user->matricula}}</td>
+                    <td>{{$user->nacionalidade}}</td>
+                    <td>{{$user->data_nascimento}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->sexo}}</td>
+                    <td>{{$user->raca}}</td>
+                    <td>{{ ControladorFormaIngresso::getTicketForm($user['id']) }}</td>
+                    <td>{{ ControladorUsuário::getCourseName($user['id']) }}</td>
+                    <td>{{$user->status}}</td>
+                    <td>{{$user->senha}}</td>
+                    <td>
+                        <a href="{{ route('usuarios.edit', $user['id']) }}" class="btn btn-sm btn-primary">Editar</a>
+
+                        <form action="{{ route('usuarios.destroy', $user['id']) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" class="btn btn-sm btn-danger" value="Apagar">
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endif
+    </div>
+</div>
+<div class="card-footer">
+    <a href="/usuarios/create" class="btn bt-sm btn-primary" role="button">Novo usuário</a>
+</div>
+@endsection
